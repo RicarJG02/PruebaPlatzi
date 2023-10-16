c63bef2ad4f3e8a96f913e2841ac13d36ea35a0e
Ricardo Guerrero <Ricarguerrero02@gmail.com>
2023-10-16 16:27:22 -0600

    Initial Commit

---
+++ PruebaPlatzi/PruebaPlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaPlatziApp.swift
+//  PruebaPlatzi
+//
+//  Created by Ricardo Guerrero Godínez on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PruebaPlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
